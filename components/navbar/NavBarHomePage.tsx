d0e31a5f9de5b665b60dd4faa43819bd9b799e36
VathanakSol <vathanaksol1605@gmail.com>
2024-12-20 12:50:35 +0700

    change new link url for document

--- components/navbar/NavBarHomePage.tsx
+++ components/navbar/NavBarHomePage.tsx
@@ -35,7 +35,7 @@ const NavBarHomePage = () => {
     const navItems = [
         { path: "/", label: "Home" },
         { path: "/service", label: "Service" },
-        { path: "https://cloudinator-document.psa-khmer.world/", label: "Document" },
+        { path: "https://cloudinator-document.soben.me", label: "Document" },
         { path: "/start-building", label: "Start Building" },
         { path: "/about", label: "About" },
     ];
