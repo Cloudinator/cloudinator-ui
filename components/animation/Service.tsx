e57578add2d03fe04e9e01d0423e658c64faa398
VathanakSol <vathanaksol1605@gmail.com>
2024-12-23 08:31:53 +0700

    fix style in home page

--- components/animation/Service.tsx
+++ components/animation/Service.tsx
@@ -3,6 +3,8 @@ import { motion } from 'framer-motion';
 import dynamic from 'next/dynamic';
 import {ServiceProps} from "@/components/animation/type";
 import {fadeInUp} from "@/components/animation/animate";
+import Link from 'next/link';
+import { Button } from '../ui/button';
 
 
 const Lottie = dynamic(() => import('react-lottie'), { ssr: false });
@@ -20,12 +22,14 @@ const Service: React.FC<ServiceProps> = ({
             <h2 className={`text-3xl md:text-4xl font-bold tracking-tight bg-gradient-to-r from-${gradientFrom} to-${gradientTo} bg-clip-text text-transparent`}>
                 {title}
             </h2>
-            <p className="text-gray-600 text-base md:text-lg leading-relaxed">
+            <p className="text-gray-600 text-base md:text-lg leading-relaxed dark:text-gray-400">
                 {description}
             </p>
-            <button className="bg-purple-500 hover:bg-purple-600 text-white px-4 md:px-6 py-2 md:py-3 rounded-lg font-medium transition-colors">
-                See More
-            </button>
+            <Button className="mt-4 bg-gradient-to-r from-purple-600 to-pink-600 dark:text-white px-8 py-4"> 
+                <Link href="/service">
+                    See More
+                </Link>
+            </Button>
         </div>
         <div className={`relative flex justify-center ${reverse ? 'md:order-1' : ''}`}>
             <Lottie options={animationOptions} height={300} width={300} />
